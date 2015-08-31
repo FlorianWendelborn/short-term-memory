e4e6fca5bd5cc6bf09d44fc1a0a9bffa071b538b
Florian Wendelborn <florianwendgithub@gmail.com>
2015-08-31 11:13:30 +0200

    added in-line documentation

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
 'use strict';
 
-function Storage (options) {
-    this.data = [];
-    this.duration = options.duration;
-}
-Storage.prototype.add = function (key, value) {
-    var that = this;
-    this.data[key] = {
-        value: value,
-        timeout: setTimeout(function () {
-            delete that.data[key];
-        }, this.duration)
-    };
-};
-
-Storage.prototype.get = function (key) {
-    if (this.data[key]) {
-        var value = this.data[key].value;
-        clearTimeout(this.data[key].timeout);
-        delete this.data[key];
-        return value;
-    } else {
-        return false;
-    }
-};
+var Storage = require('./lib/storage.js');
 
 // export
 module.exports = Storage;
